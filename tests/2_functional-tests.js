8996ec7660a755b23432637fa02b7be75ed2dda9
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-02-13 05:21:02 -0300

    solution - Run Functional Tests on an API Response using Chai-HTTP III - PUT method

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -35,9 +35,12 @@ suite("Functional Tests", function () {
       chai
         .request(server)
         .put("/travellers")
-
+        .send({ surname: "Colombo" })
         .end(function (err, res) {
-          assert.fail();
+          assert.equal(res.status, 200);
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
 
           done();
         });
