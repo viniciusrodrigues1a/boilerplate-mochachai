b9e696d4adc6f5b74dfeea13b8646753c11b5eb8
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2021-02-13 05:34:09 -0300

    solution - Run Functional Tests using a Headless Browser

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -76,11 +76,16 @@ suite("Functional Tests with Zombie.js", function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('submit "surname" : "Colombo" - write your e2e test...', function (done) {
-      browser.fill("surname", "Colombo").pressButton("submit", function () {
-        assert.fail();
+      browser
+        .fill('surname', 'Colombo')
+        .pressButton('submit', function () {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo')
+          browser.assert.text('span#surname', 'Colombo')
+          browser.assert.element('span#dates', 1)
 
-        done();
-      });
+          done();
+        });
     });
     // #6
     test('submit "surname" : "Vespucci" - write your e2e test...', function (done) {
